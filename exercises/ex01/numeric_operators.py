91f1407f12e67721ad4d6f1e07c6808a8531a13c
victor Olofsson <vctorem@email.unc.edu>
2021-08-29 12:30:37 -0400

    ex01

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -3,7 +3,8 @@
 left: str = input("left-hand side: ")
 right: str = input("right hand side: ")
 
+function: str = str(int(left) / int(right))
 
 __author__ = "730449914"
 
-print(int("is") + (int(left) / int(right)))
+print(left + " / " + right + " is " + function)
