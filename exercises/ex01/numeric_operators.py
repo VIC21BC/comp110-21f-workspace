971f9dfcc9ca6a9408f43077827cbe73ae7f9524
victor Olofsson <vctorem@email.unc.edu>
2021-08-29 15:59:15 -0400

    ex01 update 2

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,10 +1,18 @@
-"""numerical 11"""
+"""Numerical operators, left and right"""
 
 left: str = input("left-hand side: ")
 right: str = input("right hand side: ")
+left_01 = int(left)
+right_01 = int(right)
 
-function: str = str(int(left) / int(right))
+function_01: str = str(left_01 ** right_01)
+function_02: str = str(left_01 / right_01)
+function_03: str = str(left_01 // right_01)
+function_04: str = str(left_01 % right_01)
 
 __author__ = "730449914"
 
-print(left + " / " + right + " is " + function)
+print(left + " ** " + right + " is " + function_01)
+print(left + " / " + right + " is " + function_02)
+print(left + " // " + right + " is " + function_03)
+print(left + " % " + right + " is " + function_04)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,7 +1,18 @@
-"""relational operators"""
+"""False and True questions"""
 
 left: str = input("left-hand side: ")
 right: str = input("right hand side: ")
+left_01 = int(left)
+right_01 = int(right)
 
+function_01: str = str(left < right)
+function_02: str = str(left == right)
+function_03: str = str(left >= right)
+function_04: str = str(left != right)
 
 __author__ = "730449914"
+
+print(left + " < " + right + " is " + function_01)
+print(left + " == " + right + " is " + function_02)
+print(left + " >= " + right + " is " + function_03)
+print(left + " != " + right + " is " + function_04)
